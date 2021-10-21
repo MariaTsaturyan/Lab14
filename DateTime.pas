85145063e0ee5492d44bf3e91318518baf7aaf98
Мария Цатурян <mtsaturian@sfedu.ru>
2021-10-21 15:06:17 +0300

    Добавили новые функции

--- DateTime.pas
+++ DateTime.pas
@@ -4,6 +4,20 @@ begin
     IsLeapYear := True;
 end;
 
+function DaysInYear(N: integer): integer;
+begin
+  if IsLeapYear(N) = true then 
+    Result := 366
+  else
+    Result := 365; 
+end;
+
+function DaysInYearRange(A, B:integer): integer;
+Begin
+ for var I:= A to B do
+    Result += DaysInYear(I);
+end;
+
 Begin
   
 end.
